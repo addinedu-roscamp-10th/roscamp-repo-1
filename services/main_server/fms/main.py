b0dc8a1e213272b0c256a0ca1559f795ed264b67
machinity <machinity@gmail.com>
2026-05-04 12:08:42 +0900

    Fix: FMS map file path update

--- services/main_server/fms/main.py
+++ services/main_server/fms/main.py
@@ -11,7 +11,7 @@ from PIL import Image
 from pydantic import BaseModel
 from fms.robot_manager import fleet
 
-MAP_PGM  = "/home/addinedu/roscamp-repo-1/src/devices/sshopy/common/src/pinky_pro/pinky_navigation/maps/moosinsa_map.pgm"
+MAP_PGM  = "/home/team1/roscamp-repo-1/src/devices/sshopy/common/src/pinky_pro/pinky_navigation/maps/moosinsa_map.pgm"
 MAP_META = {"resolution": 0.020, "origin": [-0.276, -0.229], "width": 103, "height": 56}
 
 
